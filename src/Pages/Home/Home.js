9a6a549dfa4a72850723574a7a7afa2eb4996f95
Masud8851 <masud1971.cse@gmail.com>
2022-02-10 15:23:39 +0600

    Services Section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,27 +1,34 @@
 import React from "react";
 import "./Home.css";
 import homeBanner from "../../images/pizza-banner-1.png";
+import Services from "../Services/Services";
 
 const Home = () => {
 	return (
-		<div className="home-banner">
-			<div className="home-bg">
-				<div className="container">
-					<div className="row justify-content-center align-items-md-center align-items-end">
-						<div className="col-md-6">
-							<div className="text-box">
-								<h1>
-									QUALITY F<span>OO</span>DS
-								</h1>
-								<p>HEALTHY FOOD FOR HEALTHY BODY</p>
+		<div>
+			{/* Banner */}
+			<div className="home-banner">
+				<div className="home-bg">
+					<div className="container">
+						<div className="row justify-content-center align-items-md-center align-items-end">
+							<div className="col-md-6">
+								<div className="text-box">
+									<h1>
+										QUALITY F<span>OO</span>DS
+									</h1>
+									<p>HEALTHY FOOD FOR HEALTHY BODY</p>
+								</div>
 							</div>
-						</div>
-						<div className="col-md-6">
-							<img className="img-fluid" src={homeBanner} alt="" />
+							<div className="col-md-6">
+								<img className="img-fluid" src={homeBanner} alt="pizza" />
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+
+			{/* Services */}
+			<Services />
 		</div>
 	);
 };
